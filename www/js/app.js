51aa0e96eb37a9e7a737ddfa45e99fc4c1a0fd86
Stivenson Rincón <stivenson.rpm@gmail.com>
2016-06-26 23:37:35 -0500

    initial structure and welcome view

---
+++ www/js/app.js
@@ -0,0 +1,108 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var app = angular.module('starter', ['ionic', 'ionic-material']);
+
+app.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+})
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+
+    // Examples
+
+    .state('appexample', {
+        url: '/appexample',
+        abstract: true,
+        templateUrl: 'templates/examples/menu.html',
+        controller: 'AppexampleCtrl'
+    })
+
+    .state('appexample.lists', {
+        url: '/lists',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/examples/lists.html',
+                controller: 'ListsCtrl'
+            }
+        }
+    })
+
+    .state('appexample.ink', {
+        url: '/ink',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/examples/ink.html',
+                controller: 'InkCtrl'
+            }
+        }
+    })
+
+    .state('appexample.motion', {
+        url: '/motion',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/examples/motion.html',
+                controller: 'MotionCtrl'
+            }
+        }
+    })
+
+    .state('appexample.components', {
+        url: '/components',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/examples/components.html',
+                controller: 'ComponentsCtrl'
+            }
+        }
+    })
+
+    .state('appexample.extensions', {
+        url: '/extensions',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/examples/extensions.html',
+                controller: 'ExtensionsCtrl'
+            }
+        }
+    })
+
+    // Views
+
+    .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/views/menu.html',
+        controller: 'AppCtrl'
+    })
+    .state('app.welcome', {
+        url: '/welcome',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/views/welcome.html',
+                controller: 'WelcomeCtrl'
+            }
+        }
+    })
+
+
+    ;
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/welcome');
+});
---
+++ www/js/controllers/AppCtrl.js
@@ -0,0 +1,38 @@
+﻿app.controller('AppCtrl', function ($scope, $ionicModal, $ionicPopover, $timeout) {
+    // Form data for the login modal
+    $scope.loginData = {};
+
+    var navIcons = document.getElementsByClassName('ion-navicon');
+    for (var i = 0; i < navIcons.length; i++) {
+        navIcons.addEventListener('click', function () {
+            this.classList.toggle('active');
+        });
+    }
+
+    var fab = document.getElementById('fab');
+    fab.addEventListener('click', function () {
+        //location.href = 'https://twitter.com/satish_vr2011';
+        window.open('https://www.google.com.co', '_blank');
+    });
+
+    // .fromTemplate() method
+    var template = '<ion-popover-view>' +
+                    '   <ion-header-bar>' +
+                    '       <h1 class="title">Sobre esto</h1>' +
+                    '   </ion-header-bar>' +
+                    '   <ion-content class="padding">' +
+                    '       Esta aplicación fue creada por los hermanos Rincón para cambiar el mundo' +
+                    '   </ion-content>' +
+                    '</ion-popover-view>';
+
+    $scope.popover = $ionicPopover.fromTemplate(template, {
+        scope: $scope
+    });
+    $scope.closePopover = function () {
+        $scope.popover.hide();
+    };
+    //Cleanup the popover when we're done with it!
+    $scope.$on('$destroy', function () {
+        $scope.popover.remove();
+    });
+});
